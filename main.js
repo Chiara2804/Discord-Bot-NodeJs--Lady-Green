23218b77da096efa6707e5962870209403980b1a
Chiara2804 <67204795+Chiara2804@users.noreply.github.com>
2022-04-26 22:28:05 +0200

    Setup bot

---
+++ main.js
@@ -0,0 +1,12 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+
+client.once('ready', () => {
+    console.log('Lady Green is online!');
+});
+
+
+
+
+client.login('OTY4NjAzODAwMTExODI5MDEy.YmhQlQ.ZNo1HohiZ_xlS0s-kinJiWqJ3OM');
