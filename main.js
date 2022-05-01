f7430a4e7ab6cac768d6ae90f9e5ae22d427cb6a
Chiara Mancarella <67204795+Chiara2804@users.noreply.github.com>
2022-05-01 23:06:32 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-const client = new Discord.Client();
+const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
 
 
 client.once('ready', () => {
