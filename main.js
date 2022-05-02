84fe846c43256aa4017ef287dc66cd968ca51893
Chiara Mancarella <67204795+Chiara2804@users.noreply.github.com>
2022-05-02 19:11:43 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,24 @@
 const Discord = require('discord.js');
 const client = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
 
+const prefix = "-";
 
 client.once('ready', () => {
     console.log('Lady Green is online!');
 });
 
+client.on('message', message => {
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
 
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    if (command === 'ping') {
+        message.channel.send('Pong! 😛🏓');
+    } else if (command === 'help') {
+        message.channel.send('Ciao! Sono Lady Green. 🍃🌻');
+    }
+});
 
 
 client.login('your-token');
