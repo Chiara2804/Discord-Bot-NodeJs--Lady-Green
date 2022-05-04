8b65b7db25cc24c4c2a257ef6b881a0eb279be27
Chiara Mancarella <67204795+Chiara2804@users.noreply.github.com>
2022-05-04 22:59:35 +0200

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -2,7 +2,15 @@ module.exports = {
     name: "help",
     description: "this is a help command!",
     execute(message, args, Discord) {
-        const rules = new Discord.MessageEmbed().setColor('#93C47D').setTitle("LIST OF COMMANDS").addFields({ value: '\n-ping \n-rules \n-help \n-start \n-author' });
+        const rules = new Discord.MessageEmbed().setColor('#93C47D')
+            .setTitle("🦸‍♀️ HELP")
+            .addFields({ name: 'Commands', value: '\n-ping \n-rules \n-help \n-start \n-author', inline: true })
+            .addFields({ name: 'Rules', value: 'No spamming\nBe respectful\nNo spamming\nHave fun!', inline: true })
+            .addFields({ name: 'Guide to the channel', value: '#some values...' })
+            .addFields({ name: 'About me:', value: 'I am a bot made for Progetto NERD. I hope to be useful to you to be more eco-friendly.' })
+            .addFields({ name: 'ID', value: '960788911624425542', inline: true })
+            .addFields({ name: 'Username', value: 'Lady Green', inline: true })
+            .addFields({ name: 'Authors', value: 'Chiara mancarella\nMatilde Ruffino\nIlaria Boscolo Nata' });
         message.channel.send(rules);
     }
 }
